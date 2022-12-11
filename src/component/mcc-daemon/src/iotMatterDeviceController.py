390e273363f136846e3144a4968af96eb923a223
oidebrett <oideibrett@lccgmail.com>
2022-12-11 19:24:57 +0000

    introduced customer encoders for parsing chip-repl responses

--- src/component/mcc-daemon/src/iotMatterDeviceController.py
+++ src/component/mcc-daemon/src/iotMatterDeviceController.py
@@ -1,35 +1,36 @@
+import argparse
+import asyncio
+import atexit
+import builtins
+import json
+import logging
+import os
+import re
+import subprocess
 import sys
 import time
-import asyncio
-import subprocess
-import json
-import re
 from pprint import pformat
-from rich import print
-from rich.pretty import pprint
-from rich import pretty
-from rich import inspect
-from rich.console import Console
-import logging
-from chip import ChipDeviceCtrl
-import chip.clusters as Clusters
-from chip.ChipStack import *
-import coloredlogs
-import chip.logging
-import argparse
-import builtins
-import chip.FabricAdmin
-import chip.CertificateAuthority
-import chip.native
-import chip.discovery
-from chip.utils import CommissioningBuildingBlocks
-import atexit
+from base64 import b64encode, b64decode
 
-_fabricAdmins = None
+import chip.CertificateAuthority
+import chip.clusters as Clusters
+import chip.discovery
+import chip.FabricAdmin
+import chip.logging
+import chip.native
+import coloredlogs
+from chip import ChipDeviceCtrl
+from chip.ChipStack import *
+from chip.utils import CommissioningBuildingBlocks
+from chip.clusters.Types import Nullable, NullValue
+from rich import inspect, pretty, print
+from rich.console import Console
+from rich.pretty import pprint
+
 
 class MatterDeviceController(object):
     args = None
-    commissionedDevices = []
+    commissionableDevices = set()
     fabricDevices = set()
     MAX_DEVICES = 10
 
@@ -43,23 +44,8 @@ class MatterDeviceController(object):
         print(msg, file=sys.stdout)
         sys.stderr.flush()
 
-    def discoverDevices(self):
-        self.lPrint("Discovering devices")
-        for nodeId in range(1,self.MAX_DEVICES+1):
-            try:
-                err = devCtrl.ResolveNode(int(nodeId))
-                if err == 0:
-                    ret = devCtrl.GetAddressAndPort(int(nodeId))
-                    if ret == None:
-                        self.lPrint("Get address and port failed: " + str(nodeId))
-                    else:
-                        self.commissionedDevices.append(nodeId)
-                else:
-                    self.lPrint("Resolve node failed [{}]".format(err))
-            except:
-                self.lPrint("No discovered devices on nodeId:" + str(nodeId))
-
-        return self.commissionedDevices
+    def getFabricId(self):
+        return devCtrl.GetCompressedFabricId()
 
     def discoverFabricDevices(self):
         # Discovery happens through mdns, which means we need to wait for responses to come back.
@@ -78,8 +64,14 @@ class MatterDeviceController(object):
 
         return list(self.fabricDevices)
 
+    def discoverCommissionableDevices(self):
+        # Discovery happens through mdns, which means we need to wait for responses to come back.
+        self.lPrint("Querying for commissionable devices ...")
+        self.commissionableDevices = devCtrl.DiscoverCommissionableNodes(filterType=chip.discovery.FilterType.LONG_DISCRIMINATOR, filter=3840, stopOnFirst=False, timeoutSecond=2)
+        #print(devices)
+        return list(self.commissionableDevices)
+
     def getCommissionedDevices(self):
-        #return self.commissionedDevices
         return list(self.fabricDevices)
 
     def commissionDevice(self, ipAddress, nodeId=None):
@@ -96,9 +88,8 @@ class MatterDeviceController(object):
             builtins.devCtrl.CommissionIP(ipAddress, 20202021, nodeId)
 
             #Commented Out support for byte type of IpAddress
-            #ipAddressAsBytes = str.encode(ipAddress)
+            #ipAddressAsBytes = str.encode(ipAddress)0
             #builtins.devCtrl.CommissionIP(ipAddressAsBytes, 20202021, nodeId)
-            #self.commissionedDevices.append(nodeId)
             self.fabricDevices.add(nodeId)
             time.sleep(10)
             return nodeId
@@ -106,9 +97,21 @@ class MatterDeviceController(object):
             self.lPrint("Commission failed: ", e.message)
             return -1
 
+
+
+    def readEndpointZeroAsJsonStr(self, nodeId):
+        self.lPrint('Start Reading Endpoint0 Attributes')
+        data = (asyncio.run(devCtrl.ReadAttribute(nodeId, [0])))
+        self.lPrint('End Reading Endpoint0 Attributes')
+
+        jsonStr = self.jsonDumps(data)
+        self.lPrint(jsonStr)
+        return jsonStr
+
     def readDevAttributesAsJsonStr(self, nodeId):
         self.lPrint('Start Reading Dev Attributes')
         data = (asyncio.run(devCtrl.ReadAttribute(nodeId, [Clusters.OnOff])))
+        #data = (asyncio.run(devCtrl.ReadAttribute(nodeId, [('*')])))
         self.lPrint('End Reading Dev Attributes')
         jsonStr = self.jsonDumps(data)
         self.lPrint(jsonStr)
@@ -125,6 +128,95 @@ class MatterDeviceController(object):
         time.sleep(2)
 
     def jsonDumps(self, dm):
+        class Base64Encoder(json.JSONEncoder):
+            # pylint: disable=method-hidden
+            def default(self, o):
+                if isinstance(o, bytes):
+                    return b64encode(o).decode() #Note we will be able to get back to bytes using b64decode(o)
+                return o.__dict__ 
+            
+        def iterator(jsonStr, d):
+            if isinstance(d, dict):
+                for k, v in d.items():
+                    if isinstance(v, dict):
+                        if isinstance(k, int):
+                            jsonStr = jsonStr + "\"" + str(k) + "\"" + ": {"
+                        else:
+                            jsonStr = jsonStr + str(k) + ": {"
+
+                        jsonStr = iterator(jsonStr, v)
+                    else:
+                        jsonStr = jsonStr + "{0} : {1}".format(k, json.dumps(v, cls=Base64Encoder)) + ","
+            elif isinstance(d, list):
+                for item in d:
+                    jsonStr = iterator(jsonStr, item)
+            else: 
+                jsonStr = jsonStr + json.dumps(d, cls=Base64Encoder)
+
+            return jsonStr + "},"
+
+        jsonStr = ""
+        jsonStr = iterator(jsonStr, dm)
+        jsonStr = jsonStr.replace(" ", "")
+        jsonStr = jsonStr.replace("\n", "")
+        jsonStr = jsonStr.replace("<class'", "\"")
+        jsonStr = jsonStr.replace("\'>", "\"")
+        jsonStr = jsonStr.replace("False", "false")
+        jsonStr = jsonStr.replace("True", "true")
+        jsonStr = jsonStr.replace("Null", "null")        
+        jsonStr = jsonStr.replace(",}", "}")
+        jsonStr = jsonStr.rstrip(',')
+        jsonStr = "{" + jsonStr
+        return jsonStr
+
+
+    def jsonDumps1(self, dm):
+        dmstr = pformat(dm)
+        return dmstr
+
+
+        #internal function to repeatedly do object translation
+        def dumpObject(objectStr):
+            #to handle lists with commas first replace the commas with semicolons
+            #objectStr = re.sub(r",(?=[^\[]*\])", ";", objectStr)
+            #to handle lists with commas then re-replace the semicolons with commas
+            #outStr = re.sub(r";(?=[^\[]*\])", ",", outStr)
+
+            #fix the incorrect byte string quote i.e. hardwareAddress=b"\x14-'\xde"b'8\x9b',
+            #objectStr = re.sub(r"(b\"((.*?)*)\")+", "b'\g<2>'", objectStr)
+            #objectStr = json.dumps(objectStr)
+            #print(objectStr)
+            outStr = objectStr
+
+            #res = objectStr
+            res = dict(item.split("=") for item in objectStr.split(","))
+
+#            for item in objectStr.split(","):
+#                print(item)
+
+            #outStr = json.dumps(res)
+
+            return outStr
+
+        #internal function to repeatedly do object translation
+        def dumpObject1(result):
+            outStr = ''
+            objParams = []
+            for i in range(2,len(result.groups())+1):
+                if ('=' not in result.group(i)):
+                    objParams.append(result.group(i))
+            
+            separator = ""
+            for j in range(0,len(objParams),2):
+                key = objParams[j]
+                value = objParams[j+1]
+                outStr = outStr + separator + '"'+key+'"'+ ' : ' + (value if value.isdigit() else '"' + value+ '"') 
+                separator = ","
+
+            outStr = "{" + outStr + "}"
+
+            return outStr
+
         #this method is required as dumps doesnt work with advanced types
         dmstr = pformat(dm)
         dmstr = dmstr.replace(" ", "")
@@ -134,15 +226,80 @@ class MatterDeviceController(object):
         dmstr = dmstr.replace("False", "false")
         dmstr = dmstr.replace("True", "true")
         dmstr = dmstr.replace("Null", "null")
+        '''
         dmstr = dmstr.replace("<OnOffStartUpOnOff.kOff:0>", "0")
         dmstr = dmstr.replace("<OnOffStartUpOnOff.kOn:1>", "1")
+        dmstr = dmstr.replace("<Privilege.kAdminister:5>", "5")
+        dmstr = dmstr.replace("<AuthMode.kCase:2>", "2")
+        dmstr = dmstr.replace("<CalendarType.kBuddhist:0>", "0")
+        dmstr = dmstr.replace("<CalendarType.kChinese:1>", "1")
+        dmstr = dmstr.replace("<CalendarType.kCoptic:2>", "2")
+        dmstr = dmstr.replace("<CalendarType.kEthiopian:3>", "3")
+        dmstr = dmstr.replace("<CalendarType.kGregorian:4>", "4")
+        dmstr = dmstr.replace("<CalendarType.kHebrew:5>", "5")
+        dmstr = dmstr.replace("<CalendarType.kIndian:6>", "6")
+        dmstr = dmstr.replace("<CalendarType.kIslamic:7>", "7")
+        dmstr = dmstr.replace("<CalendarType.kJapanese:8>", "8")
+        dmstr = dmstr.replace("<CalendarType.kKorean:9>", "9")
+        dmstr = dmstr.replace("<CalendarType.kPersian:10>", "10")
+        dmstr = dmstr.replace("<CalendarType.kTaiwanese:11>", "11")
+        dmstr = dmstr.replace("<TempUnit.kFahrenheit:0>", "0")
+        dmstr = dmstr.replace("<BatChargeLevel.kOk:0>", "0")
+        dmstr = dmstr.replace("<BatReplaceability.kUnspecified:0>", "0")
+        dmstr = dmstr.replace("<RegulatoryLocationType.kIndoor:0>", "0")
         dmstr = dmstr.replace("1>", "1")
-        line = re.sub(r"(\d+)(:)", "\"\g<1>\":", dmstr)
-        return line
+        '''
+
+        #replace the enums like <RegulatoryLocationType.kIndoor:0>
+        dmstr = re.sub(r"(<\w+.\w+)(:(\d+))>", "\g<3>", dmstr)
+
+        #dmstr = re.sub(r"(b'(.*?)*')+", "\"\g<0>\"", dmstr)
+
+        #put quotes around byte strings
+        #dmstr = re.sub("=b'","=\"b'", dmstr)
+
+
+        '''
+        #Replace any single quotes with double quotes
+        dmstr = re.sub("'","\"", dmstr)
+
+
+        #fix the incorrect byte string quote i.e. hardwareAddress=b"\x14-'\xde"b'8\x9b',
+        #dmstr = re.sub(r"(b\"((.*?)*)\")+", "b'\g<2>'", dmstr)
+
+        #Replace all the non standard objects so that they can be handled by json
+        #pattern = r"([A-Z])\w+(\w*)\(((\w*)=(\d*\w*(\"(b'(.*?))*'\")*)(,(\w+)=(\d*\w*(\"(b'(.*?))*'\")*(\[\d*(,(\d*))*\])*))+)\)"
+        #pattern = r"([A-Z])\w+(\w*)\(((\w*)=(\"*\'*\d*\w*\"*\'*(\"(b'(.*?))*'\")*)(,(\w+)=(\"*\'*\d*\w*\"*\'*(\"(b'(.*?))*'\")*(\[\d*(\"(b'(.*?))*'\")*(,(\"(b'(.*?))*'\")*(\d*))*\])*))+)\)"
+
+        #temporarily strip  out all bytes strings
+        dmstr = re.sub(r"(b'(.*?)*')+", "", dmstr)
+        #temporarily strip  out all bytes strings
+        dmstr = re.sub(r"(b\"(.*?)*\")+", "", dmstr)
+
+        pattern = r"\w+\((.*?)\)"
+        result = re.search(pattern, dmstr)
+        while(result is not None):
+            #print(result.groups())
+            #print("Group 2")
+            #print(result.group(2))
+            #print("Group 3")
+            #print(result.group(3))
+            #print(dumpObject(result.group(1)))
+            #print("Group 0")
+            #print(result.group(0))
+            #print("Group 1")
+            #print(result.group(1))
+            #print(dumpObject(result.group(1)))
+            dmstr = dmstr.replace(result.group(0), dumpObject(result.group(1)))
+            result = re.search(pattern, dmstr)
+
+        '''
+        dmstr = re.sub(r"(\d+)(:)", "\"\g<1>\":", dmstr)
+        return dmstr
 
     def testDumps(self, dm):
         #this method is required as dumps doesnt work with advanced types
-        dmstr = pprint.pformat(dm)
+        dmstr = pformat(dm)
         return dmstr
 
     def cleanStart(self):
