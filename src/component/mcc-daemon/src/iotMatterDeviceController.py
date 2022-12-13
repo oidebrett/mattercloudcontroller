83b91f1eb3510763861be81b8553f5b391fc3c7c
oidebrett <oideibrett@lccgmail.com>
2022-12-13 20:12:26 +0000

    refactoring of json parser to handle CommissionableNode types

--- src/component/mcc-daemon/src/iotMatterDeviceController.py
+++ src/component/mcc-daemon/src/iotMatterDeviceController.py
@@ -27,7 +27,6 @@ from rich import inspect, pretty, print
 from rich.console import Console
 from rich.pretty import pprint
 
-
 class MatterDeviceController(object):
     args = None
     commissionableDevices = set()
@@ -133,6 +132,25 @@ class MatterDeviceController(object):
             def default(self, o):
                 if isinstance(o, bytes):
                     return b64encode(o).decode() #Note we will be able to get back to bytes using b64decode(o)
+                if isinstance(o, chip.ChipDeviceCtrl.CommissionableNode):
+                    return {
+                        "addresses": o.addresses, 
+                        "commissioningMode": o.commissioningMode,
+                        "deviceName": o.deviceName ,
+                        "deviceType": o.deviceType,
+                        "hostName": o.hostName ,
+                        "instanceName": o.instanceName ,
+                        "longDiscriminator": o.longDiscriminator ,
+                        "mrpRetryIntervalActive": o.mrpRetryIntervalActive ,
+                        "mrpRetryIntervalIdle": o.mrpRetryIntervalIdle ,
+                        "pairingHint": o.pairingHint ,
+                        "pairingInstruction": o.pairingInstruction ,
+                        "port": o.port,
+                        "productId": o.productId,
+                        "supportsTcp": o.supportsTcp,
+                        "vendorId": o.vendorId
+                        }
+                    
                 return o.__dict__ 
             
         def iterator(jsonStr, d):
@@ -149,7 +167,7 @@ class MatterDeviceController(object):
                         jsonStr = jsonStr + "{0} : {1}".format(k, json.dumps(v, cls=Base64Encoder)) + ","
             elif isinstance(d, list):
                 for item in d:
-                    jsonStr = iterator(jsonStr, item)
+                    jsonStr = "[" + iterator(jsonStr, item) + "]"
             else: 
                 jsonStr = jsonStr + json.dumps(d, cls=Base64Encoder)
 
@@ -163,144 +181,22 @@ class MatterDeviceController(object):
         jsonStr = jsonStr.replace("\'>", "\"")
         jsonStr = jsonStr.replace("False", "false")
         jsonStr = jsonStr.replace("True", "true")
-        jsonStr = jsonStr.replace("Null", "null")        
+        jsonStr = jsonStr.replace("Null", "null")
         jsonStr = jsonStr.replace(",}", "}")
+        jsonStr = jsonStr.replace(",]", "]")
         jsonStr = jsonStr.rstrip(',')
         jsonStr = "{" + jsonStr
+
+        #to handle the extra curly put in for a list we will remove now - fix this later
+        jsonStr = jsonStr.replace("{[", "[")
+        jsonStr = jsonStr.replace("}}]}", "}]")
+
+        #to handle the extra curly put in for a standard commissionable node entry we will remove now- fix this later
+        result = re.search(r"(^{{)(.*?)(}})", jsonStr)
+        if (result is not None):
+            jsonStr = jsonStr.replace(result.group(0), "{"+result.group(2)+"}")
         return jsonStr
 
-
-    def jsonDumps1(self, dm):
-        dmstr = pformat(dm)
-        return dmstr
-
-
-        #internal function to repeatedly do object translation
-        def dumpObject(objectStr):
-            #to handle lists with commas first replace the commas with semicolons
-            #objectStr = re.sub(r",(?=[^\[]*\])", ";", objectStr)
-            #to handle lists with commas then re-replace the semicolons with commas
-            #outStr = re.sub(r";(?=[^\[]*\])", ",", outStr)
-
-            #fix the incorrect byte string quote i.e. hardwareAddress=b"\x14-'\xde"b'8\x9b',
-            #objectStr = re.sub(r"(b\"((.*?)*)\")+", "b'\g<2>'", objectStr)
-            #objectStr = json.dumps(objectStr)
-            #print(objectStr)
-            outStr = objectStr
-
-            #res = objectStr
-            res = dict(item.split("=") for item in objectStr.split(","))
-
-#            for item in objectStr.split(","):
-#                print(item)
-
-            #outStr = json.dumps(res)
-
-            return outStr
-
-        #internal function to repeatedly do object translation
-        def dumpObject1(result):
-            outStr = ''
-            objParams = []
-            for i in range(2,len(result.groups())+1):
-                if ('=' not in result.group(i)):
-                    objParams.append(result.group(i))
-            
-            separator = ""
-            for j in range(0,len(objParams),2):
-                key = objParams[j]
-                value = objParams[j+1]
-                outStr = outStr + separator + '"'+key+'"'+ ' : ' + (value if value.isdigit() else '"' + value+ '"') 
-                separator = ","
-
-            outStr = "{" + outStr + "}"
-
-            return outStr
-
-        #this method is required as dumps doesnt work with advanced types
-        dmstr = pformat(dm)
-        dmstr = dmstr.replace(" ", "")
-        dmstr = dmstr.replace("\n", "")
-        dmstr = dmstr.replace("<class'", "\"")
-        dmstr = dmstr.replace("\'>", "\"")
-        dmstr = dmstr.replace("False", "false")
-        dmstr = dmstr.replace("True", "true")
-        dmstr = dmstr.replace("Null", "null")
-        '''
-        dmstr = dmstr.replace("<OnOffStartUpOnOff.kOff:0>", "0")
-        dmstr = dmstr.replace("<OnOffStartUpOnOff.kOn:1>", "1")
-        dmstr = dmstr.replace("<Privilege.kAdminister:5>", "5")
-        dmstr = dmstr.replace("<AuthMode.kCase:2>", "2")
-        dmstr = dmstr.replace("<CalendarType.kBuddhist:0>", "0")
-        dmstr = dmstr.replace("<CalendarType.kChinese:1>", "1")
-        dmstr = dmstr.replace("<CalendarType.kCoptic:2>", "2")
-        dmstr = dmstr.replace("<CalendarType.kEthiopian:3>", "3")
-        dmstr = dmstr.replace("<CalendarType.kGregorian:4>", "4")
-        dmstr = dmstr.replace("<CalendarType.kHebrew:5>", "5")
-        dmstr = dmstr.replace("<CalendarType.kIndian:6>", "6")
-        dmstr = dmstr.replace("<CalendarType.kIslamic:7>", "7")
-        dmstr = dmstr.replace("<CalendarType.kJapanese:8>", "8")
-        dmstr = dmstr.replace("<CalendarType.kKorean:9>", "9")
-        dmstr = dmstr.replace("<CalendarType.kPersian:10>", "10")
-        dmstr = dmstr.replace("<CalendarType.kTaiwanese:11>", "11")
-        dmstr = dmstr.replace("<TempUnit.kFahrenheit:0>", "0")
-        dmstr = dmstr.replace("<BatChargeLevel.kOk:0>", "0")
-        dmstr = dmstr.replace("<BatReplaceability.kUnspecified:0>", "0")
-        dmstr = dmstr.replace("<RegulatoryLocationType.kIndoor:0>", "0")
-        dmstr = dmstr.replace("1>", "1")
-        '''
-
-        #replace the enums like <RegulatoryLocationType.kIndoor:0>
-        dmstr = re.sub(r"(<\w+.\w+)(:(\d+))>", "\g<3>", dmstr)
-
-        #dmstr = re.sub(r"(b'(.*?)*')+", "\"\g<0>\"", dmstr)
-
-        #put quotes around byte strings
-        #dmstr = re.sub("=b'","=\"b'", dmstr)
-
-
-        '''
-        #Replace any single quotes with double quotes
-        dmstr = re.sub("'","\"", dmstr)
-
-
-        #fix the incorrect byte string quote i.e. hardwareAddress=b"\x14-'\xde"b'8\x9b',
-        #dmstr = re.sub(r"(b\"((.*?)*)\")+", "b'\g<2>'", dmstr)
-
-        #Replace all the non standard objects so that they can be handled by json
-        #pattern = r"([A-Z])\w+(\w*)\(((\w*)=(\d*\w*(\"(b'(.*?))*'\")*)(,(\w+)=(\d*\w*(\"(b'(.*?))*'\")*(\[\d*(,(\d*))*\])*))+)\)"
-        #pattern = r"([A-Z])\w+(\w*)\(((\w*)=(\"*\'*\d*\w*\"*\'*(\"(b'(.*?))*'\")*)(,(\w+)=(\"*\'*\d*\w*\"*\'*(\"(b'(.*?))*'\")*(\[\d*(\"(b'(.*?))*'\")*(,(\"(b'(.*?))*'\")*(\d*))*\])*))+)\)"
-
-        #temporarily strip  out all bytes strings
-        dmstr = re.sub(r"(b'(.*?)*')+", "", dmstr)
-        #temporarily strip  out all bytes strings
-        dmstr = re.sub(r"(b\"(.*?)*\")+", "", dmstr)
-
-        pattern = r"\w+\((.*?)\)"
-        result = re.search(pattern, dmstr)
-        while(result is not None):
-            #print(result.groups())
-            #print("Group 2")
-            #print(result.group(2))
-            #print("Group 3")
-            #print(result.group(3))
-            #print(dumpObject(result.group(1)))
-            #print("Group 0")
-            #print(result.group(0))
-            #print("Group 1")
-            #print(result.group(1))
-            #print(dumpObject(result.group(1)))
-            dmstr = dmstr.replace(result.group(0), dumpObject(result.group(1)))
-            result = re.search(pattern, dmstr)
-
-        '''
-        dmstr = re.sub(r"(\d+)(:)", "\"\g<1>\":", dmstr)
-        return dmstr
-
-    def testDumps(self, dm):
-        #this method is required as dumps doesnt work with advanced types
-        dmstr = pformat(dm)
-        return dmstr
 
     def cleanStart(self):
         if os.path.isfile('/tmp/repl-storage.json'):
