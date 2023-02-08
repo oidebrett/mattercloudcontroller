838ae7085d55c17796f71f8ce308a40a36f59525
oidebrett <oidebrett@lccgmail.com>
2023-02-08 19:55:04 +0000

    updated parser to allow for interactions and actions and also changed to connectedhomeip repo regarding parsing test scripts

--- src/component/mcc-daemon/src/actionParser.py
+++ src/component/mcc-daemon/src/actionParser.py
@@ -8,21 +8,45 @@ import os
 #Set the paths up so we are using the parsing in the connectedhomeip repo
 import config 
 sys.path.append(os.path.abspath(config.chipDir+"/scripts/py_matter_yamltests/"))
+
+from matter_yamltests.definitions import SpecDefinitions
+from matter_yamltests.parser import TestParserConfig
 import matter_yamltests.parser 
 
 class ActionParser:
-    def __init__(self, data, pics_file, definitions):
+    def __init__(self, data: str, parser_config: TestParserConfig = TestParserConfig()):
+
+        matter_yamltests.parser._TESTS_SECTION.append('actions') #Lets add the option for actions
+
         matter_yamltests.parser._check_valid_keys(data, matter_yamltests.parser._TESTS_SECTION)
 
         self.name = matter_yamltests.parser._value_or_none(data, 'name')
         self.PICS = matter_yamltests.parser._value_or_none(data, 'PICS')
 
-        self._parsing_config_variable_storage = matter_yamltests.parser._value_or_none(data, 'config')
+        config = data.get('config', {})
+        for key, value in parser_config.config_override.items():
+            if value is None:
+                continue
 
-        pics_checker = matter_yamltests.parser.PICSChecker(pics_file)
-        tests = matter_yamltests.parser._value_or_none(data, 'tests')
+            if isinstance(config[key], dict) and 'defaultValue' in config[key]:
+                config[key]['defaultValue'] = value
+            else:
+                config[key] = value
+        self._parsing_config_variable_storage = config
+
+        # These are a list of "KnownVariables". These are defaults the codegen used to use. This
+        # is added for legacy support of tests that expect to uses these "defaults".
+        self.__populate_default_config_if_missing('nodeId', 0x12345)
+        self.__populate_default_config_if_missing('endpoint', '')
+        self.__populate_default_config_if_missing('cluster', '')
+        self.__populate_default_config_if_missing('timeout', '90')
+
+        pics_checker = matter_yamltests.parser.PICSChecker(parser_config.pics)
+        tests = matter_yamltests.parser._value_or_none(data, 'actions')
+        print(tests)
         self.tests = matter_yamltests.parser.YamlTests(
-            self._parsing_config_variable_storage, definitions, pics_checker, tests)
+            self._parsing_config_variable_storage, parser_config.definitions, pics_checker, tests)
 
-    def update_config(self, key, value):
-        self._parsing_config_variable_storage[key] = value
+    def __populate_default_config_if_missing(self, key, value):
+        if key not in self._parsing_config_variable_storage:
+            self._parsing_config_variable_storage[key] = value
