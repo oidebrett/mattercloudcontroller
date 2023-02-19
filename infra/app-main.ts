b6e2bfca12840708717da18f7c0902d7c0ff4fb9
oidebrett <oideibrett@lccgmail.com>
2023-02-19 16:29:12 +0000

    Added a monitoring stack to post shadow updates to SNS topic - still to get webhook working

--- infra/app-main.ts
+++ infra/app-main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { AppContext } from '../lib/template/app-context';
 import { ThingInstallerStack } from './stack/iot/thing-installer-stack';
+import { ThingMonitorStack } from './stack/iot/thing-monitor-stack';
 import { ComponentUploadStack } from './stack/greengrass/component-upload-stack';
 import { ComponentDeploymentStack } from './stack/greengrass/component-deployment-stack';
 import { ApiGatewayDeploymentStack } from './stack/api-gateway/api-gateway-stack';
@@ -11,6 +12,7 @@ const appContext = new AppContext({
 
 if (appContext.stackCommonProps != undefined) {
     new ThingInstallerStack(appContext, appContext.appConfig.Stack.ThingInstaller);
+    new ThingMonitorStack(appContext, appContext.appConfig.Stack.ThingMonitor);
     new ComponentUploadStack(appContext, appContext.appConfig.Stack.ComponentUpload);
     new ComponentDeploymentStack(appContext, appContext.appConfig.Stack.ComponentDeployment);
     new ApiGatewayDeploymentStack(appContext, appContext.appConfig.Stack.ApiGatewayDeployment)
